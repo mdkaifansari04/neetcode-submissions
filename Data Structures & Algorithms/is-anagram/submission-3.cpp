7b83463430b2eb8f9c300355d8baec90b08a7ccf
Md Kaif Ansari <117475603+mdkaifansari04@users.noreply.github.com>
2026-09-09 15:28:44 +0530

    Add: is-anagram - submission-3

---
+++ Data Structures & Algorithms/is-anagram/submission-3.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        // int sCount = 0, tCount = 0;
+
+        // for(char c: s){
+        //     sCount+= (int)c;
+        // }
+        
+        // for(char c: t){
+        //     tCount+= (int)c;
+        // }
+        // return sCount == tCount;
+
+
+    unordered_map<char, int> sMap;
+    unordered_map<char, int> tMap;
+
+    for (char i : s)
+        sMap[i]++;
+
+    for (char i : t)
+        tMap[i]++;
+
+    return sMap == tMap;
+    }
+
+
+    // the most optimal approach 
+    // sort(s.begin(), s.end());
+    // sort(t.begin(), t.end());
+
+    // return s == t;
+};
